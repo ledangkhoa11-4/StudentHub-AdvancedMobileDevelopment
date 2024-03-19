fa9553f25aa53e49acae145c2985beff8e794dde
PhongTran1804 <trannguyenphongg@gmail.com>
2024-03-19 15:28:23 +0700

    show project list

--- lib/presentation/5_browse_project_flow/project_item.dart
+++ lib/presentation/5_browse_project_flow/project_item.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ProjectItem extends StatelessWidget {
+class ProjectItem extends StatefulWidget {
   final String createdDate;
   final String title;
   final String timeDuration;
   final int numberOfStudents;
-  final String description;
+  final List<String> descriptions; // Now a list of descriptions
   final String proposal;
+  final bool isLiked;
 
   const ProjectItem({
     Key? key,
@@ -14,12 +15,29 @@ class ProjectItem extends StatelessWidget {
     required this.title,
     required this.timeDuration,
     required this.numberOfStudents,
-    required this.description,
+    required this.descriptions, // Update here
     required this.proposal,
+    required this.isLiked,
   }) : super(key: key);
 
   @override
+  _ProjectItemState createState() => _ProjectItemState();
+}
+
+class _ProjectItemState extends State<ProjectItem> {
+  bool isLiked = false;
+
+  void toggleLike() {
+    setState(() {
+      isLiked = !isLiked;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Color greenColor = Color.fromARGB(255, 48, 121, 51); // Green color
+    final Color grayColor = const Color.fromARGB(255, 134, 132, 132);
+
     return Card(
       margin: const EdgeInsets.all(8.0),
       child: Padding(
@@ -28,21 +46,54 @@ class ProjectItem extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              title,
-              style: Theme.of(context).textTheme.headline6,
+              'Created: ${widget.createdDate}',
+              style: TextStyle(
+                // fontStyle: FontStyle.italic,
+                color: grayColor,
+              ),
             ),
-            SizedBox(height: 8.0),
-            Text('Created: $createdDate'),
-            Text('Duration: $timeDuration'),
-            Text('Students needed: $numberOfStudents'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    widget.title,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6!
+                        .copyWith(color: greenColor),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                      widget.isLiked ? Icons.favorite : Icons.favorite_border),
+                  color: widget.isLiked ? Colors.red : null,
+                  onPressed: toggleLike,
+                ),
+              ],
+            ),
+            Text(
+                'Time: ${widget.timeDuration}' +
+                    ', ${widget.numberOfStudents} students needed',
+                style: TextStyle(color: grayColor)),
             SizedBox(height: 8.0),
             Text(
               'Students are looking for:',
               style: Theme.of(context).textTheme.subtitle1,
             ),
-            Text(description),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: widget.descriptions
+                  .map((description) => Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Text('• $description'),
+                      ))
+                  .toList(),
+            ),
             SizedBox(height: 8.0),
-            Text('Proposal: $proposal'),
+            Text('Proposal: ${widget.proposal}',
+                style: TextStyle(color: grayColor)),
           ],
         ),
       ),
--- lib/presentation/5_browse_project_flow/project_list.dart
+++ lib/presentation/5_browse_project_flow/project_list.dart
@@ -1,40 +1,160 @@
 import 'package:flutter/material.dart';
-import 'project_item.dart'; // Ensure this import is correct based on your project structure
+import 'project_item.dart';
 
-class ProjectList extends StatelessWidget {
-  final List<Map<String, dynamic>> projects = [
+class ProjectList extends StatefulWidget {
+  ProjectList({Key? key}) : super(key: key);
+
+  @override
+  _ProjectListState createState() => _ProjectListState();
+}
+
+class _ProjectListState extends State<ProjectList> {
+  final List<Map<String, dynamic>> _allProjects = [
     {
       'createdDate': '2024-01-01',
       'title': 'Senior Mobile Dev',
       'timeDuration': '1-3 months',
       'numberOfStudents': 4,
-      'description': 'Looking for experienced mobile developers.',
+      'descriptions': [
+        'Looking for experienced mobile developers.',
+        'Experience with Flutter is a plus.'
+      ],
       'proposal': 'Less than 5 months',
+      'isLiked': true,
     },
-    // Add more projects here
+    {
+      'createdDate': '2024-02-15',
+      'title': 'Junior Web Developer',
+      'timeDuration': '2-4 months',
+      'numberOfStudents': 3,
+      'descriptions': [
+        'Seeking junior web developers with a knack for front-end design.',
+        'Familiarity with React or Vue preferred.'
+      ],
+      'proposal': 'Up to 4 months',
+      'isLiked': false,
+    },
+    {
+      'createdDate': '2024-03-10',
+      'title': 'Data Science Enthusiast',
+      'timeDuration': '3-6 months',
+      'numberOfStudents': 2,
+      'descriptions': [
+        'Looking for data science students passionate about machine learning.',
+        'Experience with Python and basic ML libraries required.'
+      ],
+      'proposal': '3 to 6 months engagement',
+      'isLiked': true,
+    },
+    {
+      'createdDate': '2024-04-05',
+      'title': 'UX/UI Design Intern',
+      'timeDuration': '1-2 months',
+      'numberOfStudents': 1,
+      'descriptions': [
+        'Seeking a UX/UI design intern with a good eye for modern app design.',
+        'Portfolio of past design projects preferred.'
+      ],
+      'proposal': '1 to 2 months',
+      'isLiked': false,
+    },
   ];
 
-  ProjectList({Key? key}) : super(key: key);
+  List<Map<String, dynamic>> _filteredProjects = [];
+  bool _showLikedOnly = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredProjects = _allProjects;
+  }
+
+  void _toggleShowLiked() {
+    setState(() {
+      _showLikedOnly = !_showLikedOnly;
+      _filterProjects();
+    });
+  }
+
+  void _filterProjects({String query = ''}) {
+    final lowerCaseQuery = query.toLowerCase();
+    setState(() {
+      if (_showLikedOnly) {
+        _filteredProjects = _allProjects.where((project) {
+          final titleLower = project['title'].toLowerCase();
+          final matchesQuery = titleLower.contains(lowerCaseQuery);
+          return project['isLiked'] && matchesQuery;
+        }).toList();
+      } else {
+        _filteredProjects = _allProjects.where((project) {
+          final titleLower = project['title'].toLowerCase();
+          return titleLower.contains(lowerCaseQuery);
+        }).toList();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Text('Project List'),
       ),
-      body: ListView.builder(
-        itemCount: projects.length,
-        itemBuilder: (context, index) {
-          final project = projects[index];
-          return ProjectItem(
-            createdDate: project['createdDate'] as String,
-            title: project['title'] as String,
-            timeDuration: project['timeDuration'] as String,
-            numberOfStudents: project['numberOfStudents'] as int,
-            description: project['description'] as String,
-            proposal: project['proposal'] as String,
-          );
-        },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 38,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: TextField(
+                      onChanged: (value) => _filterProjects(query: value),
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search, color: Colors.black),
+                        hintText: 'Search...',
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(color: Colors.black),
+                        contentPadding: EdgeInsets.symmetric(vertical: 0),
+                      ),
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                      _showLikedOnly ? Icons.favorite : Icons.favorite_border),
+                  onPressed: _toggleShowLiked,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredProjects.length,
+              itemBuilder: (context, index) {
+                final project = _filteredProjects[index];
+                return ProjectItem(
+                  createdDate: project['createdDate'] as String,
+                  title: project['title'] as String,
+                  timeDuration: project['timeDuration'] as String,
+                  numberOfStudents: project['numberOfStudents'] as int,
+                  descriptions:
+                      (project['descriptions'] as List).cast<String>(),
+                  proposal: project['proposal'] as String,
+                  isLiked: project['isLiked'] as bool,
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
