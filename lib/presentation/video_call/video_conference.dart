5f8e07b1016f3db5dbc6e4519a9ecc3611f038ab
PhongTran1804 <trannguyenphongg@gmail.com>
2024-04-30 18:06:22 +0700

    update

--- lib/presentation/video_call/video_conference.dart
+++ lib/presentation/video_call/video_conference.dart
@@ -28,42 +28,44 @@ class VideoConferencePageState extends State<VideoConferencePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: ZegoUIKitPrebuiltVideoConference(
-          appID: 1913282325,
-          appSign:
-              "8f09796184ed6a1c74005b54f19cacde780cd4dbde7f0a763f6ef528e7702699",
-          conferenceID: widget.conferenceID,
-          userID: localUserID,
-          userName: "user_$localUserID",
-          config: ZegoUIKitPrebuiltVideoConferenceConfig(
-            turnOnCameraWhenJoining: false,
-            audioVideoViewConfig:
-                ZegoPrebuiltAudioVideoViewConfig(showCameraStateOnView: false),
-            topMenuBarConfig: ZegoTopMenuBarConfig(
-              buttons: [ZegoMenuBarButtonName.showMemberListButton],
-            ),
-            bottomMenuBarConfig: ZegoBottomMenuBarConfig(
-              buttons: [
-                ZegoMenuBarButtonName.toggleMicrophoneButton,
-                ZegoMenuBarButtonName.leaveButton,
-                ZegoMenuBarButtonName.switchAudioOutputButton,
-              ],
-            ),
-            avatarBuilder: (BuildContext context, Size size,
-                ZegoUIKitUser? user, Map extraInfo) {
-              return user != null
-                  ? Container(
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            'https://media.istockphoto.com/id/1302783988/vector/the-embarrassed-man.jpg?s=612x612&w=0&k=20&c=bIPvdEHEGAP0RnSH5n45dvHfsqvZKv8NwG5qjRWCNTg=',
-                          ),
-                        ),
-                      ),
-                    )
-                  : const SizedBox();
-            },
-          )),
+        appID: 1913282325,
+        appSign:
+            "8f09796184ed6a1c74005b54f19cacde780cd4dbde7f0a763f6ef528e7702699",
+        conferenceID: widget.conferenceID,
+        userID: localUserID,
+        userName: "user_$localUserID",
+        config: ZegoUIKitPrebuiltVideoConferenceConfig(),
+        // config: ZegoUIKitPrebuiltVideoConferenceConfig(
+        //   turnOnCameraWhenJoining: false,
+        //   audioVideoViewConfig:
+        //       ZegoPrebuiltAudioVideoViewConfig(showCameraStateOnView: false),
+        //   topMenuBarConfig: ZegoTopMenuBarConfig(
+        //     buttons: [ZegoMenuBarButtonName.showMemberListButton],
+        //   ),
+        //   bottomMenuBarConfig: ZegoBottomMenuBarConfig(
+        //     buttons: [
+        //       ZegoMenuBarButtonName.toggleMicrophoneButton,
+        //       ZegoMenuBarButtonName.leaveButton,
+        //       ZegoMenuBarButtonName.switchAudioOutputButton,
+        //     ],
+        //   ),
+        //   avatarBuilder: (BuildContext context, Size size,
+        //       ZegoUIKitUser? user, Map extraInfo) {
+        //     return user != null
+        //         ? Container(
+        //             decoration: const BoxDecoration(
+        //               shape: BoxShape.circle,
+        //               image: DecorationImage(
+        //                 image: NetworkImage(
+        //                   'https://media.istockphoto.com/id/1302783988/vector/the-embarrassed-man.jpg?s=612x612&w=0&k=20&c=bIPvdEHEGAP0RnSH5n45dvHfsqvZKv8NwG5qjRWCNTg=',
+        //                 ),
+        //               ),
+        //             ),
+        //           )
+        //         : const SizedBox();
+        //   },
+        // )
+      ),
     );
   }
 }
