8cd10a2de4163d12b93dbc199fc93589a288368f
PhongTran1804 <trannguyenphongg@gmail.com>
2024-05-04 11:31:07 +0700

    switch branch

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,20 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <pasteboard/pasteboard_plugin.h>
-#include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <syncfusion_pdfviewer_windows/syncfusion_pdfviewer_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
-#include <zego_express_engine/zego_express_engine_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   PasteboardPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PasteboardPlugin"));
-  PermissionHandlerWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   SyncfusionPdfviewerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SyncfusionPdfviewerWindowsPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
-  ZegoExpressEnginePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ZegoExpressEnginePlugin"));
 }
