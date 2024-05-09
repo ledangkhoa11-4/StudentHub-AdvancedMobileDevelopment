a9ea97583d1f28966872992c1a987df568e7ad29
PhongTran1804 <trannguyenphongg@gmail.com>
2024-05-09 21:32:27 +0700

    search local

--- lib/presentation/message/message.dart
+++ lib/presentation/message/message.dart
@@ -1,6 +1,7 @@
 import 'package:boilerplate/core/widgets/progress_indicator_widget.dart';
 import 'package:boilerplate/data/sharedpref/shared_preference_helper.dart';
 import 'package:boilerplate/di/service_locator.dart';
+import 'package:boilerplate/domain/entity/chat/chat.dart';
 import 'package:boilerplate/domain/entity/chat/chatUser.dart';
 import 'package:boilerplate/domain/entity/user/user.dart';
 import 'package:boilerplate/presentation/app_bar/app_bar.dart';
@@ -20,6 +21,7 @@ class MessageScreen extends StatefulWidget {
 
 class _MessageScreenState extends State<MessageScreen> {
   final UserStore _userStore = getIt<UserStore>();
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -32,7 +34,10 @@ class _MessageScreenState extends State<MessageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: UserAppBar.buildAppBar(context, titleWidget: Text(AppLocalizations.of(context).translate('message'))),
+      appBar: UserAppBar.buildAppBar(
+        context,
+        titleWidget: Text(AppLocalizations.of(context).translate('message')),
+      ),
       bottomNavigationBar:
           UserNavigationBar.buildNavigationBar(context, setState: setState),
       body: Stack(
@@ -45,8 +50,13 @@ class _MessageScreenState extends State<MessageScreen> {
                 Padding(
                   padding: EdgeInsets.only(top: 16, left: 16, right: 16),
                   child: TextField(
+                    controller: _searchController,
+                    onChanged: (value) {
+                      setState(() {});
+                    },
                     decoration: InputDecoration(
-                      hintText:  AppLocalizations.of(context).translate('search'),
+                      hintText:
+                          AppLocalizations.of(context).translate('search'),
                       hintStyle: TextStyle(color: Colors.grey.shade600),
                       prefixIcon: Icon(
                         Icons.search,
@@ -57,82 +67,144 @@ class _MessageScreenState extends State<MessageScreen> {
                       fillColor: Colors.grey.shade100,
                       contentPadding: EdgeInsets.all(8),
                       focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide(color: Colors.grey.shade100)),
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.grey.shade100),
+                      ),
                       enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide(color: Colors.grey.shade100)),
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.grey.shade100),
+                      ),
                     ),
                   ),
                 ),
                 Observer(builder: (context) {
                   final Set<int> groupProject = _userStore.allChatList != null
                       ? _userStore.allChatList!
-                          .where((chat) => chat.project != null)
+                          .where((chat) =>
+                              chat.sender.fullname.toLowerCase().contains(
+                                  (_searchController.text ?? '')
+                                      .toLowerCase()) ||
+                              chat.receiver.fullname.toLowerCase().contains(
+                                  (_searchController.text ?? '').toLowerCase()))
                           .map((chat) => chat.project!.id!)
                           .toSet()
-                      : Set.of([]);
-                  return Column(
-                    children: [
-                      ...groupProject.map((id) {
-                        final project = _userStore.allChatList!
-                            .firstWhere((chat) => chat.project?.id == id)
-                            .project;
-                        return ExpansionTile(
-                          leading: Icon(BootstrapIcons.kanban),
-                          collapsedShape: RoundedRectangleBorder(
-                            side: BorderSide.none,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide.none,
-                          ),
-                          title: Text(
-                            "${AppLocalizations.of(context).translate('project')} ${project != null ? project.title : "N/A"}",
-                            style: TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.bold),
-                          ),
-                          children: [
-                            Observer(builder: (context) {
-                              final currentProfile =
-                                  getIt<SharedPreferenceHelper>()
-                                      .currentProfile;
-                              final chatsInProject = _userStore.allChatList!
-                                  .where((chat) => chat.project?.id == id)
-                                  .toList();
-                              return ListView.builder(
-                                itemCount: chatsInProject.length,
-                                shrinkWrap: true,
-                                physics: NeverScrollableScrollPhysics(),
-                                itemBuilder: (context, index) {
-                                  final chat = chatsInProject[index];
-                                  return ConversationList(
-                                    name: chat.sender.id == _userStore.user!.id
-                                        ? chat.receiver.fullname
-                                        : chat.sender.fullname,
-                                    messageText:
-                                        chat.sender.id == _userStore.user!.id
-                                            ? "${AppLocalizations.of(context).translate('you')}: ${chat.content}"
-                                            : chat.content,
-                                    imageUrl:
-                                        currentProfile == UserRole.COMPANY.value
-                                            ? "https://i.imgur.com/ugcoGNH.png"
-                                            : "https://i.imgur.com/SR6SaqF.png",
-                                    time: "${Moment(chat.createdAt).fromNow()}",
-                                    isMessageRead: false,
-                                    projectId: chat.project!.id!,
-                                    userId:
-                                        chat.sender.id == _userStore.user!.id
-                                            ? chat.receiver.id
-                                            : chat.sender.id,
-                                  );
-                                },
-                              );
-                            })
-                          ],
-                        );
-                      }).toList(),
-                    ],
+                      : Set<int>();
+
+                  final filteredChats = _userStore.allChatList != null
+                      ? _userStore.allChatList!
+                          .where((chat) =>
+                              chat.sender.fullname.toLowerCase().contains(
+                                  (_searchController.text ?? '')
+                                      .toLowerCase()) ||
+                              chat.receiver.fullname.toLowerCase().contains(
+                                  (_searchController.text ?? '').toLowerCase()))
+                          .toList()
+                      : <ChatEntity>[];
+
+                  if (filteredChats.isEmpty) {
+                    return Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "No chat found",
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                  }
+
+                  if (_searchController.text == "") {
+                    return Column(
+                      children: [
+                        ...groupProject.map((id) {
+                          final project = _userStore.allChatList!
+                              .firstWhere((chat) => chat.project?.id == id)
+                              .project;
+                          return ExpansionTile(
+                            leading: Icon(BootstrapIcons.kanban),
+                            collapsedShape: RoundedRectangleBorder(
+                              side: BorderSide.none,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide.none,
+                            ),
+                            title: Text(
+                              "${AppLocalizations.of(context).translate('project')} ${project != null ? project.title : "N/A"}",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.bold),
+                            ),
+                            children: [
+                              Observer(builder: (context) {
+                                final currentProfile =
+                                    getIt<SharedPreferenceHelper>()
+                                        .currentProfile;
+                                final chatsInProject = _userStore.allChatList!
+                                    .where((chat) => chat.project?.id == id)
+                                    .toList();
+                                return ListView.builder(
+                                  itemCount: chatsInProject.length,
+                                  shrinkWrap: true,
+                                  physics: NeverScrollableScrollPhysics(),
+                                  itemBuilder: (context, index) {
+                                    final chat = chatsInProject[index];
+                                    return ConversationList(
+                                      name:
+                                          chat.sender.id == _userStore.user!.id
+                                              ? chat.receiver.fullname
+                                              : chat.sender.fullname,
+                                      messageText: chat.sender.id ==
+                                              _userStore.user!.id
+                                          ? "${AppLocalizations.of(context).translate('you')}: ${chat.content}"
+                                          : chat.content,
+                                      imageUrl: currentProfile ==
+                                              UserRole.COMPANY.value
+                                          ? "https://i.imgur.com/ugcoGNH.png"
+                                          : "https://i.imgur.com/SR6SaqF.png",
+                                      time:
+                                          "${Moment(chat.createdAt).fromNow()}",
+                                      isMessageRead: false,
+                                      projectId: chat.project!.id!,
+                                      userId:
+                                          chat.sender.id == _userStore.user!.id
+                                              ? chat.receiver.id
+                                              : chat.sender.id,
+                                    );
+                                  },
+                                );
+                              })
+                            ],
+                          );
+                        }).toList(),
+                      ],
+                    );
+                  }
+                  // PHONG ----------------------------------------------------------------------
+                  final currentProfile =
+                      getIt<SharedPreferenceHelper>().currentProfile;
+                  return ListView.builder(
+                    itemCount: filteredChats.length,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      final chat = filteredChats[index];
+                      return ConversationList(
+                        name: chat.sender.id == _userStore.user!.id
+                            ? chat.receiver.fullname
+                            : chat.sender.fullname,
+                        messageText: chat.sender.id == _userStore.user!.id
+                            ? "${AppLocalizations.of(context).translate('you')}: ${chat.content}"
+                            : chat.content,
+                        imageUrl: currentProfile == UserRole.COMPANY.value
+                            ? "https://i.imgur.com/ugcoGNH.png"
+                            : "https://i.imgur.com/SR6SaqF.png",
+                        time: "${Moment(chat.createdAt).fromNow()}",
+                        isMessageRead: false,
+                        projectId: chat.project!.id!,
+                        userId: chat.sender.id == _userStore.user!.id
+                            ? chat.receiver.id
+                            : chat.sender.id,
+                      );
+                    },
                   );
+                  // ---------------------------------------------------------------------------------
                 }),
               ],
             ),
